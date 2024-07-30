833077f6902a072e9123c65e4f43fe38df69a306
Blackops Security <51173880+blackopsinc@users.noreply.github.com>
2024-07-30 14:04:55 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+apt-get update && apt-get upgrade
+apt-get -y install systemctl
 curl -k 'https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb' > /tmp/amazon-ssm-agent.deb 
 dpkg -i /tmp/amazon-ssm-agent.deb
 systemctl enable amazon-ssm-agent
